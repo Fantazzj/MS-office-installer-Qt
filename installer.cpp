d284cc57a58161364ab502f5b6e1efc3a36e4cff
Fantazzj <63418260+Fantazzj@users.noreply.github.com>
2022-09-14 17:00:15 +0200

    format applied

--- installer.cpp
+++ installer.cpp
@@ -1,16 +1,18 @@
 #include "installer.h"
+#include "OfficeDeploymentTool.h"
 #include "ui_installer.h"
 
-Installer::Installer(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::Installer)
-{
+Installer::Installer(QWidget* parent) :
+    QMainWindow(parent), ui(new Ui::Installer) {
     ui->setupUi(this);
 
+    ui->checkBoxWord->setChecked(true);
+    ui->checkBoxPowerPoint->setChecked(true);
+    ui->checkBoxExcel->setChecked(true);
+
+    OfficeDeploymentTool office = OfficeDeploymentTool();
 }
 
-Installer::~Installer()
-{
+Installer::~Installer() {
     delete ui;
 }
-
--- installer.h
+++ installer.h
@@ -4,18 +4,19 @@
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class Installer; }
+namespace Ui {
+    class Installer;
+}
 QT_END_NAMESPACE
 
-class Installer : public QMainWindow
-{
+class Installer : public QMainWindow {
     Q_OBJECT
 
 public:
-    Installer(QWidget *parent = nullptr);
+    Installer(QWidget* parent = nullptr);
     ~Installer();
 
 private:
-    Ui::Installer *ui;
+    Ui::Installer* ui;
 };
-#endif // INSTALLER_H
+#endif// INSTALLER_H
