c73a7521c3ae655c191abec28104081f1ac4a225
Fantazzj <63418260+Fantazzj@users.noreply.github.com>
2023-03-02 18:22:31 +0100

    translation updates

--- MS-office-installer_it_IT.ts
+++ MS-office-installer_it_IT.ts
@@ -1,3 +1,102 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="it_IT"></TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>Installer</name>
+    <message>
+        <location filename="installer.ui" line="14"/>
+        <source>Installer</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="installer.ui" line="29"/>
+        <source>Office setup</source>
+        <translation>Setup di Office</translation>
+    </message>
+    <message>
+        <location filename="installer.ui" line="42"/>
+        <source>...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="installer.ui" line="62"/>
+        <source>Access</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="installer.ui" line="72"/>
+        <source>Project</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="installer.ui" line="88"/>
+        <source>Word</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="installer.ui" line="98"/>
+        <source>SharePointDesigner</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="installer.ui" line="105"/>
+        <source>InfoPath</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="installer.ui" line="121"/>
+        <source>Excel</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="installer.ui" line="140"/>
+        <source>Publisher</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="installer.ui" line="159"/>
+        <source>PowerPoint</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="installer.ui" line="169"/>
+        <source>Groove</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="installer.ui" line="176"/>
+        <source>Lync</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="installer.ui" line="183"/>
+        <source>Outlook</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="installer.ui" line="199"/>
+        <source>Teams</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="installer.ui" line="209"/>
+        <source>OneDrive</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="installer.ui" line="218"/>
+        <source>Install</source>
+        <translation>Installa</translation>
+    </message>
+    <message>
+        <location filename="installer.ui" line="233"/>
+        <source>Choose presets</source>
+        <translation>Scegli preset</translation>
+    </message>
+    <message>
+        <location filename="installer.ui" line="257"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
