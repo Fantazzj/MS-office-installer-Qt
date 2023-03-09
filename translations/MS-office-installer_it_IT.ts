25b74761c487648bc3de50e9d7e74bc2656accb0
Fantazzj <63418260+Fantazzj@users.noreply.github.com>
2023-03-09 17:29:36 +0100

    auto creation of translations

--- translations/MS-office-installer_it_IT.ts
+++ translations/MS-office-installer_it_IT.ts
@@ -5,158 +5,208 @@
     <name>Installer</name>
     <message>
         <location filename="../src/installer.ui" line="14"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="392"/>
         <source>Installer</source>
-        <translation type="unfinished"></translation>
+        <translation>Installatore</translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="29"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="393"/>
         <source>Choose product languages</source>
-        <translation type="unfinished"></translation>
+        <translation>Scegli le lingue per i prodotti</translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="36"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="394"/>
         <source>Choose proofing languages</source>
-        <translation type="unfinished"></translation>
+        <translation>Scegli lingue per l&apos;autocorrettore</translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="56"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="395"/>
         <source>OneNote</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="72"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="396"/>
         <source>Access</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="82"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="397"/>
         <source>Outlook</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="89"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="398"/>
         <source>OneDrive</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="105"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="399"/>
         <source>Word</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="115"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="400"/>
         <source>SharePointDesigner</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="122"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="401"/>
         <source>Lync</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="129"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="402"/>
         <source>Project</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="145"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="403"/>
         <source>Teams</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="164"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="404"/>
         <source>PowerPoint</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="183"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="405"/>
         <source>Excel</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="202"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="406"/>
         <source>Publisher</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="212"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="407"/>
         <source>Groove</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="219"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="408"/>
         <source>InfoPath</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="244"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="409"/>
         <source>Version</source>
-        <translation type="unfinished"></translation>
+        <translation>Versione</translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="279"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="410"/>
         <source>Product</source>
-        <translation type="unfinished"></translation>
+        <translation>Prodotto</translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="321"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="411"/>
         <source>Release channel</source>
-        <translation type="unfinished"></translation>
+        <translation>Canale di rilascio</translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="338"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="412"/>
         <source>Automatic updates</source>
-        <translation type="unfinished"></translation>
+        <translation>Aggiornamenti automatici</translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="361"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="413"/>
         <source>File save type</source>
-        <translation type="unfinished"></translation>
+        <translation>Tipo di files</translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="374"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="414"/>
         <source>Open XML (eg. docx)</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="387"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="415"/>
         <source>Open Document (eg. odt)</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="400"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="416"/>
         <source>Not now</source>
-        <translation type="unfinished"></translation>
+        <translation>Non ora</translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="431"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="417"/>
         <source>Office setup</source>
-        <translation type="unfinished"></translation>
+        <translation>Setup di office</translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="444"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="418"/>
         <source>...</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="455"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="419"/>
         <source>Export configuration</source>
-        <translation type="unfinished"></translation>
+        <translation>Esporta configurazione</translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="462"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="420"/>
         <source>Download</source>
-        <translation type="unfinished"></translation>
+        <translation>Scarica</translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="469"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="421"/>
         <source>Install</source>
-        <translation type="unfinished"></translation>
+        <translation>Installa</translation>
     </message>
     <message>
         <location filename="../src/installer.ui" line="476"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_installer.h" line="422"/>
         <source>Download and install</source>
-        <translation type="unfinished"></translation>
+        <translation>Scarica e installa</translation>
+    </message>
+    <message>
+        <location filename="../src/installer.cpp" line="9"/>
+        <source>Missing setup.exe</source>
+        <translation>setup.exe mancante</translation>
+    </message>
+    <message>
+        <location filename="../src/installer.cpp" line="10"/>
+        <source>There is no office&apos;s setup.exe, so you cannot install or download, only export configuration</source>
+        <translation>Non è presente alcun file setup.exe di Office, quindi non sarà possibile installare e scaricare il programa, solo esportare la configurazione</translation>
+    </message>
+</context>
+<context>
+    <name>languageSelector</name>
+    <message>
+        <location filename="../src/languageSelector.ui" line="17"/>
+        <location filename="../cmake-build-debug/MS-office-installer_autogen/include/ui_languageSelector.h" line="60"/>
+        <source>Dialog</source>
+        <translation>Dialogo!?!?!?!?</translation>
     </message>
 </context>
 </TS>
