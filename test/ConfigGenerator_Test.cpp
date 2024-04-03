7e6d4aaad2396e4fe0e9c2bb621dbd3d568c8845
Fantazzj <63418260+Fantazzj@users.noreply.github.com>
2024-04-03 16:29:46 +0200

    minor testing update

--- test/ConfigGenerator_Test.cpp
+++ test/ConfigGenerator_Test.cpp
@@ -1,13 +1,25 @@
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("Default selection", "[ConfigGenerator]") {
+TEST_CASE("Check basic tests", "[check]" ) {
 	int a = 3;
 	int b = 6;
 	SECTION("Changing") {
-		b=3;
-		CHECK(a==b);
+		b = 3;
+		CHECK(a == b);
 	}
 	SECTION("Same") {
-		CHECK_FALSE(a==b);
+		CHECK_FALSE(a == b);
+	}
+}
+
+TEST_CASE("Require basic tests", "[require]" ) {
+	int a = 3;
+	int b = 6;
+	SECTION("Changing") {
+		b = 3;
+		REQUIRE(a == b);
+	}
+	SECTION("Same") {
+		REQUIRE_FALSE(a == b);
 	}
 }
